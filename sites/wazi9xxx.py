62c3af56b6170e36baf023d9169c51c28d6314f1
Yazawazi <47273265+Yazawazi@users.noreply.github.com>
2022-04-16 22:48:51 +0800

    进行一个疯狂的写
    1. 标签和搜索结果解析器；
    2. 标签、浏览、搜索功能；
    3. 视频解析占位。

--- sites/wazi9xxx.py
+++ sites/wazi9xxx.py
@@ -33,11 +33,80 @@ class wazi9xxx:
         else:
             return soup
     
-    def parseMain(self, soup):
+    def parseMoviesList(self, soup):
+        div = soup.find("div", {
+            "class": "list_items"
+        })
+        if not div:
+            return []
+        items = div.find_all("div", {
+            "class": "movie-preview"
+        })
+        moviesList = []
+        for item in items:
+            moviesList.append({
+                "id": item.find("span", {
+                    "class": "ribbon"
+                }).attrs["data-id"],
+                "title": item.find("span", {
+                    "class": "movie-title"
+                }).text.strip(),
+                "link": item.find("span", {
+                    "class": "movie-title"
+                }).find("a").attrs["href"],
+                "name": item.find("span", {
+                    "class": "movie-title"
+                }).find("a").attrs["href"].split("/")[-2],
+            })
+        return moviesList
+    
+    def parseTagsList(self, soup):
+        div = soup.find_all("div", {
+            "class": "tags"
+        })
+        if not div:
+            return []
+        div = div[-1]
+        items = div.find_all("li")
+        tagsList = []
+        for item in items:
+            tagsList.append({
+                "id": item.get("class")[1].split("-")[-1],
+                "title": item.find("a").text.strip(),
+                "link": item.find("a").attrs["href"],
+                "name": item.find("a").attrs["href"].split("/")[-2]
+            })
+        return tagsList
+    
+    def parseSearch(self, soup):
+        return wazi9xxx.parseMoviesList(self, soup)
+    
+    def parseMainAndCategory(self, soup):
+        return (wazi9xxx.parseMoviesList(self, soup), wazi9xxx.parseTagsList(self, soup))
+    
+    def parseVideo(self, soup):
         pass
     
+    def search(self, keyword, page):
+        if page > 1:
+            url = f"{self.baseURL}page/{page}/?s={keyword}"
+        else:
+            url = f"{self.baseURL}?s={keyword}"
+        return wazi9xxx.parseSearch(self, wazi9xxx.returnSoup(self, url))
+    
+    def getVideo(self, videoId, name):
+        url = f"{self.baseURL}video_{videoId}/{name}/"
+        return wazi9xxx.parseVideo(self, wazi9xxx.returnSoup(self, url))
+    
+    def getCategory(self, category, page, sort = None):
+        if page > 1:
+            url = f"{self.baseURL}video_category/{category}/page/{page}/"
+        else:
+            url = f"{self.baseURL}video_category/{category}/"
+        if sort:
+            url += f"?sort={sort}"
+        return wazi9xxx.parseMainAndCategory(self, wazi9xxx.returnSoup(self, url))
+    
     def getPage(self, page):
-        url = self.baseURL + "page/" + str(page) + "/"
-        soup = self.returnSoup(url)
-        return soup
-
+        url = f"{self.baseURL}page/{page}/"
+        return wazi9xxx.parseMainAndCategory(self, wazi9xxx.returnSoup(self, url))
